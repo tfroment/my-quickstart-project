a9868e226801f9d68beb0b5aac4a303384ba88aa
Thomas Froment <thomas.froment@eclipse-foundation.org>
2026-01-03 19:17:18 +0100

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+ //   public String myMethodTest() {
+ //   }
+}
